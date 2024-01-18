99af6282261172fc79ce80adac24e1c8a0b051f6
projectx <loredana.dugulean@gmail.com>
2024-01-18 16:43:54 +0200

    practice files from www.learncpp.com training

--- src/chapter13/7.cpp
+++ src/chapter13/7.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 
-struct Employee
-{
-    int id {};
-    int age {};
-    double wage {};
-};
+// struct Employee
+// {
+//     int id {};
+//     int age {};
+//     double wage {};
+// };
+
+// struct Company
+// {
+//     int numberOfEmployees {};
+//     Employee CEO {}; // Employee is a struct within the Company struct
+// };
+
+
+// OR
 
 struct Company
 {
-    int numberOfEmployees {};
-    Employee CEO {}; // Employee is a struct within the Company struct
+    struct Employee // accessed via Company::Employee
+    {
+        int id{};
+        int age{};
+        double wage{};
+    };
+
+    int numberOfEmployees{};
+    Employee CEO{}; // Employee is a struct within the Company struct
 };
 
 int main()
 {
     Company myCompany{ 7, { 1, 32, 55000.0 } }; // Nested initialization list to initialize Employee
     std::cout << myCompany.CEO.wage << '\n'; // print the CEO's wage
+    std::cout << myCompany.numberOfEmployees << '\n'; // print the CEO's wage
+
+    ++myCompany.numberOfEmployees; //a new employee joined the company
     std::cout << myCompany.numberOfEmployees << '\n'; // print the CEO's wage
 
     return 0;
