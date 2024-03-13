bc67299c3c955392cc1b4f5428f47d5bb11447cb
projectx <loredana.dugulean@gmail.com>
2024-03-13 17:19:26 +0200

    added practice files from design patterns in c++ course

---
+++ src/DesignPatterns/Section3/4/Facets.cpp
@@ -0,0 +1,26 @@
+//builder facets
+// instead of just using one builder to actually work on an object,we're going to be use two builders.
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <string>
+using namespace std;
+
+#include "Person.h"
+#include "PersonBuilder.h"
+#include "PersonAddressBuilder.h"
+#include "PersonJobBuilder.h"
+
+int main__()
+{
+  Person p = Person::create()
+    .lives().at("123 London Road").with_postcode("SW1 1GB").in("London")
+    .works().at("PragmaSoft").as_a("Consultant").earning(10e6);
+
+  cout << p << endl;
+  getchar();
+  return 0;
+}
