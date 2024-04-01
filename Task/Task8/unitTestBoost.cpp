92b83859db82d6f92e56d0cee0891108030a091e
projectx <loredana.dugulean@gmail.com>
2024-04-01 11:00:05 +0300

    added the final files for Task8

--- Task/Task8/unitTestBoost.cpp
+++ Task/Task8/unitTestBoost.cpp
@@ -6,38 +6,29 @@ BOOST_AUTO_TEST_SUITE( test_suite )
 
 BOOST_AUTO_TEST_CASE( test_case1 )
 {
-  //create an instance of SingletonDatabaseCar
+  //create an instance of SingletonDatabaseCarc
   SingletonDatabaseCar &database = SingletonDatabaseCar::get();
 
   //check the number of cars for a certain brand
   //check if the number for a brand in database is correct  
   BOOST_CHECK_EQUAL(database.get_nbCar("Aston Martin"), 2);
-  BOOST_TEST_MESSAGE( "The number of brand ASton Martin in the database is correct" );
+  BOOST_TEST_MESSAGE( "TEST CASE 1: The number of brand ASton Martin in the database is correct" );
 
   BOOST_CHECK_EQUAL(database.get_nbCar("Toyota"), 1789);
-  BOOST_TEST_MESSAGE( "The number of brand Toyota in the database is correct" );  
+  BOOST_TEST_MESSAGE( "TEST CASE 1: The number of brand Toyota in the database is correct" );  
 }
 
 BOOST_AUTO_TEST_CASE( test_case2 )
-{
+{ 
   //create an instance of SingletonDatabaseCar
-  SingletonDatabaseCar &database = SingletonDatabaseCar::get();
+  SingletonDatabaseCar &database = SingletonDatabaseCar::get();   
  
   //check if the car brand is not in the database will return 0 
   BOOST_CHECK_EQUAL(database.get_nbCar("BMW"), 0);
-  BOOST_TEST_MESSAGE( "The BMW brand car doesn't exist in the database and it returns 0" );  
+  BOOST_TEST_MESSAGE( "TEST CASE 2: The BMW brand car doesn't exist in the database and it returns 0" );  
 }
 
 BOOST_AUTO_TEST_CASE( test_case3 )
-{
-  //create an instance of SingletonDatabaseCar
-  SingletonDatabaseCar &database = SingletonDatabaseCar::get();
-  
-  //check if the car brand is in the database but with the incorrect number of cars will give an error
-  BOOST_CHECK_EQUAL(database.get_nbCar("Mercedes"), 3);   
-}
-
-BOOST_AUTO_TEST_CASE( test_case4 )
 {
   //create an instance of SingletonDatabaseCar
   SingletonDatabaseCar &database1 = SingletonDatabaseCar::get();
@@ -46,7 +37,25 @@ BOOST_AUTO_TEST_CASE( test_case4 )
 
   //test if both instances point to the same object
   BOOST_CHECK_EQUAL(&database1, &database2);
-  BOOST_TEST_MESSAGE( "the 2 instances point to the same SingletonDatabaseCar object" );  
+  BOOST_TEST_MESSAGE( "TEST CASE 3: the 2 instances point to the same SingletonDatabaseCar object" );  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// ======================================================================================================
+
+BOOST_AUTO_TEST_SUITE( test_suite1 )
+
+BOOST_AUTO_TEST_CASE( test_case4 )
+{ 
+  //create an instance of SingletonDatabaseCar
+  SingletonDatabaseCar &database = SingletonDatabaseCar::get();   
+  
+  //check if the car brand is in the database but with the incorrect number of cars will give an error
+  if (database.get_nbCar("Mercedes") != 200)
+  {
+    BOOST_ERROR("TEST CASE 4: Test failed: the number of cars Mercedes is inccorect");
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
