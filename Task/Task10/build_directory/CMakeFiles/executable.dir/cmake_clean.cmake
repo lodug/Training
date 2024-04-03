6c33a85e08b8b3d6abb8cfbc3394c3091e277910
projectx <loredana.dugulean@gmail.com>
2024-04-03 10:16:15 +0300

    added the final files for Task10

---
+++ Task/Task10/build_directory/CMakeFiles/executable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executable.dir/src/automobil.cpp.o"
+  "CMakeFiles/executable.dir/src/main.cpp.o"
+  "CMakeFiles/executable.dir/src/masina1.cpp.o"
+  "CMakeFiles/executable.dir/src/masina2.cpp.o"
+  "executable"
+  "executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task/Task10/src/masina2.cpp
@@ -0,0 +1,20 @@
+//subtask3
+
+#include <iostream>
+#include <string>
+
+#include "../include/masina2.h"
+#include "../include/automobil.h"
+
+//constructor
+    Masina2::Masina2(std::string combustibil, std::string caroserie, std::string tractiune, int putere, std::string echipare, std::string* VIN)
+        : Automobil{ combustibil, caroserie, tractiune, putere, echipare, VIN }         
+    {
+        std::cout << "Constructing Masina2 " << '\n';
+    }
+
+//destructor
+    Masina2::~Masina2()
+    {
+        std::cout << "Destructing Masina2" << '\n';
+    }
